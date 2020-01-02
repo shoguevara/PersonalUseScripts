4e6e2051a3f04964e4bb2f22c554a6f9c4891ea6
ShoGuevara <shoguevara@gmail.com>
2020-01-03 00:17:27 +0300

    Update otp.sh

--- otp.sh
+++ otp.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-#in order this script to work oath-toolkit should be installed
+# in order this script to work oath-toolkit should be installed
+# there's also an .otpkeys file, that should be created in homedir 
+
+# create script file
+#   touch /usr/local/bin/otp
+#   chmod 751 /usr/local/bin/otp
+#   chmod +x /usr/local/bin/otp  
+#   echo 'export PATH="/usr/local/opt/curl/bin:/usr/local/sbin:$PATH"' > ~/.bash_profile
+
+# create keys file
+#   touch ~/.otpkeys
+#   sudo chown root:wheel .otpkeys
+# change permissions so only sudo user can see what's inside the file
+#   sudo chmod 751 .otpkeys
+#Fill with 'servicename=KEYCODE'
+
 scriptname=`basename $0`
 if [ -z $1 ]
 then
@@ -19,3 +34,5 @@ then
     exit
 fi
 /usr/local/bin/oathtool --totp -b $otpkey
+
+# After everything is done you can run sudo otp servicename to get your OTP keys
